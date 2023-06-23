22e01e5a552db3829d6f5d7786523c12ea722193
Lemonyte <49930425+lemonyte@users.noreply.github.com>
2023-06-23 01:13:25 -0700

    Remove invocation from __init__.py

--- deta_space_actions/__init__.py
+++ deta_space_actions/__init__.py
@@ -1,6 +1,5 @@
-from . import invocation
 from .actions import Action, Actions
 from .input import Input, InputType
 
-__all__ = ["Action", "Actions", "Input", "InputType", "invocation"]
+__all__ = ["Action", "Actions", "Input", "InputType"]
 __version__ = "0.0.1"
