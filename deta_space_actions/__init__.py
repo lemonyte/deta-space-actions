c2352c6757e3070b4bceb6d0b39103f6653b7dfb
Lemonyte <49930425+lemonyte@users.noreply.github.com>
2023-06-23 00:54:25 -0700

    Fix __init__.py

--- deta_space_actions/__init__.py
+++ deta_space_actions/__init__.py
@@ -1,5 +1,6 @@
-from . import invocation  # noqa: F401
-from .actions import Action, Actions  # noqa: F401
-from .input import Input, InputType  # noqa: F401
+from . import invocation
+from .actions import Action, Actions
+from .input import Input, InputType
 
+__all__ = ["Action", "Actions", "Input", "InputType", "invocation"]
 __version__ = "0.0.1"
