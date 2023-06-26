7c9c900a7c8f379e36e5d62de00865dfa5484fc7
Lemonyte <49930425+lemonyte@users.noreply.github.com>
2023-06-26 00:05:35 -0700

    Add ActionsMiddleware

--- deta_space_actions/actions.py
+++ deta_space_actions/actions.py
@@ -1,9 +1,23 @@
-from typing import Any, Awaitable, Callable, MutableMapping, Sequence
-
-from starlette.exceptions import HTTPException
-from starlette.requests import Request
+import json
+from typing import (
+    TYPE_CHECKING,
+    Any,
+    Awaitable,
+    Callable,
+    MutableMapping,
+    Optional,
+    Sequence,
+)
 
 from .input import Input
+
+if TYPE_CHECKING:
+    from asgiref.typing import (
+        ASGI3Application,
+        ASGIReceiveCallable,
+        ASGISendCallable,
+        Scope,
+    )
 
 # TODO: MutableMapping is incompatible with Dict??
 HandlerInput = MutableMapping[str, Any]
@@ -67,14 +81,72 @@ class Actions:
             ],
         }
 
-    async def middleware(self, request: Request, call_next):
-        # TODO: make it generic asgi with no dependencies
-        if request.method == "GET" and request.url.path == self.declaration_path:
-            return self.declaration()
-        if request.method == "POST" and request.url.path.startswith(self.base_path):
-            name = request.url.path.lstrip(self.base_path + "/").rstrip("/")
-            action = self._actions.get(name)
-            if not action:
-                raise HTTPException(404, f"Action {name} not found")
-            return await action.run(await request.json())
-        return await call_next(request)
+
+class ActionsMiddleware:
+    def __init__(self, app: "ASGI3Application", actions: Actions):
+        self.app = app
+        self.actions = actions
+
+    async def __call__(self, scope: "Scope", receive: "ASGIReceiveCallable", send: "ASGISendCallable"):
+        if scope["type"] == "http":
+            if scope["path"].rstrip("/") == self.actions.declaration_path:
+                if scope["method"] != "GET":
+                    await self.send_plain_text(send, "Method Not Allowed", status=405)
+                    return
+                await self.send_json(send, self.actions.declaration())
+                return
+            if scope["path"].startswith(self.actions.base_path):
+                if scope["method"] != "POST":
+                    await self.send_plain_text(send, "Method Not Allowed", status=405)
+                    return
+                name = scope["path"].lstrip(self.actions.base_path + "/").rstrip("/")
+                action = self.actions.get(name)
+                if not action:
+                    await self.send_plain_text(send, "Not Found", status=404)
+                    return
+                message = await receive()
+                if message["type"] == "http.request":
+                    payload = json.loads(message["body"])
+                    await self.send_json(send, await action.run(payload))
+                    return
+        await self.app(scope, receive, send)
+
+    async def send_plain_text(self, send: "ASGISendCallable", content: str, status: int = 200):
+        await send(
+            {
+                "type": "http.response.start",
+                "status": status,
+                "headers": [(b"Content-Type", b"text/plain; charset=utf-8")],
+                "trailers": False,
+            }
+        )
+        await send(
+            {
+                "type": "http.response.body",
+                "body": content.encode("utf-8"),
+                "more_body": False,
+            }
+        )
+
+    async def send_json(self, send: "ASGISendCallable", content: Any, status: int = 200):
+        await send(
+            {
+                "type": "http.response.start",
+                "status": status,
+                "headers": [(b"Content-Type", b"application/json")],
+                "trailers": False,
+            }
+        )
+        await send(
+            {
+                "type": "http.response.body",
+                "body": json.dumps(
+                    content,
+                    ensure_ascii=False,
+                    allow_nan=False,
+                    indent=None,
+                    separators=(",", ":"),
+                ).encode("utf-8"),
+                "more_body": False,
+            }
+        )
