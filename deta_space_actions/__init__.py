73300fd974bdaad8329dfd366ce2bef7dfaac1bf
Lemonyte <49930425+lemonyte@users.noreply.github.com>
2023-06-26 00:05:52 -0700

    Import ActionsMiddleware in __init__.py

--- deta_space_actions/__init__.py
+++ deta_space_actions/__init__.py
@@ -1,5 +1,5 @@
-from .actions import Action, Actions
+from .actions import Action, Actions, ActionsMiddleware
 from .input import Input, InputType
 
-__all__ = ["Action", "Actions", "Input", "InputType"]
+__all__ = ["Action", "Actions", "ActionsMiddleware", "Input", "InputType"]
 __version__ = "0.0.1"
