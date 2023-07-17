bddb81bc90de9790aec621b67e483eaaaf6b6516
Lemonyte <49930425+lemonyte@users.noreply.github.com>
2023-07-17 04:16:51 -0700

    Make some view params kw-only

--- deta_space_actions/views.py
+++ deta_space_actions/views.py
@@ -19,21 +19,22 @@ class DetailView:
     def __init__(
         self,
         text: str,
+        *,
         title: Optional[str] = None,
-        image_url: Optional[str] = None,
         url: Optional[str] = None,
+        image_url: Optional[str] = None,
     ):
         self.text = text
         self.title = title
-        self.image_url = image_url
         self.url = url
+        self.image_url = image_url
 
     def as_serializable(self):
         return {
             "title": self.title,
             "text": self.text,
-            "image_url": self.image_url,
             "url": self.url,
+            "image_url": self.image_url,
         }
 
 
@@ -41,7 +42,13 @@ class FileView:
     __slots__ = ("url", "type", "name")
     id = "@deta/file"
 
-    def __init__(self, url: str, type: str, name: Optional[str] = None):
+    def __init__(
+        self,
+        url: str,
+        type: str,
+        *,
+        name: Optional[str] = None,
+    ):
         self.url = url
         self.type = type
         self.name = name
@@ -60,6 +67,7 @@ class ListItem:
     def __init__(
         self,
         title: str,
+        *,
         description: Optional[str] = None,
         url: Optional[str] = None,
         view: Optional["View"] = None,
@@ -87,7 +95,13 @@ class ListView:
     __slots__ = ("items", "title", "description")
     id = "@deta/list"
 
-    def __init__(self, items: Sequence[ListItem], title: Optional[str] = None, description: Optional[str] = None):
+    def __init__(
+        self,
+        items: Sequence[ListItem],
+        *,
+        title: Optional[str] = None,
+        description: Optional[str] = None,
+    ):
         self.items = items
         self.title = title
         self.description = description
@@ -122,7 +136,8 @@ def custom_view(id_: str):
         >>> @actions.action(view=MyView)
         >>> async def my_action(payload) -> MyView:
         >>>     return MyView(...)
-        """
+    """
+
     class CustomView(CustomViewProto):
         id = id_
 
